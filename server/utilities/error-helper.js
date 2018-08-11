b8c79f6dc902bfa83bbca42c85b8f66a4eede00b
Mayank Patel <patelmayankce@gmail.com>
2018-08-11 16:59:02 +0530

    add utility helper

---
+++ server/utilities/error-helper.js
@@ -0,0 +1,15 @@
+'use strict'
+let Boom = require('boom')
+
+function handleError(err) {
+  if (err.isBoom) {
+    throw err
+  } else {
+    console.error(err)
+    throw Boom.badImplementation(err)
+  }
+}
+
+module.exports = {
+  handleError
+}
