5160dd74087396e9cd2034ee55dcaed167c1ab63
Mayank Patel <patelmayankce@gmail.com>
2018-08-11 16:59:54 +0530

    add auth plugin

---
+++ server/plugins/auth.plugin.js
@@ -0,0 +1,40 @@
+'use strict'
+const config = require('config')
+
+exports.plugin = {
+  async register(server, options) {
+    const jwtValidate = async (decodedToken, request, h) => {
+      let credentials = {
+        user: {}
+      }
+      const isValid = true
+      // Authentication Code will be here
+      return {
+        isValid,
+        credentials
+      }
+    }
+
+    server.auth.strategy('auth', 'jwt', {
+      key: config.constants.JWT_SECRET,
+      validate: jwtValidate,
+      verifyOptions: {
+        algorithms: ['HS256']
+      }
+    })
+
+    // Add helper method to get request ip
+    const getIP = function(request) {
+      // We check the headers first in case the server is behind a reverse proxy.
+      // see: https://ypereirareis.github.io/blog/2017/02/15/nginx-real-ip-behind-nginx-reverse-proxy/
+      return (
+        request.headers['x-real-ip'] ||
+        request.headers['x-forwarded-for'] ||
+        request.info.remoteAddress
+      )
+    }
+    server.method('getIP', getIP, {})
+  },
+  name: 'auth',
+  version: require('../../package.json').version
+}
